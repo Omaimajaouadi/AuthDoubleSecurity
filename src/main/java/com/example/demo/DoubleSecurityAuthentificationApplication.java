d845e29d273bcc3514b793774a29da092bf9dfc2
“omaima <jaouadiomaimaz@gmail.com>
2021-12-02 23:59:22 +0100

    A B

---
+++ src/main/java/com/example/demo/DoubleSecurityAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleSecurityAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleSecurityAuthentificationApplication.class, args);
+	}
+
+}
